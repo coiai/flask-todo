20b8d5bf70f7b3def2283aa92a5261b01255b86d
Coiai Chuang <Coiaichan@gmail.com>
2021-08-13 21:39:21 +0900

    編集までできました

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def dbtest():
     # データベースに命令
     cursor.execute("SELECT name, age, address FROM user WHERE id=1")
     user_info = cursor.fetchone()
-    cursor.close()
+    connect.close()
     print(user_info)
     return render_template('dbtest.html',html_info = user_info)
 
@@ -53,12 +53,51 @@ def add_post():
     cursor.execute("INSERT INTO task VALUES(null, ?)",(py_task,))
     # DB を保存
     connect.commit()
-    cursor.close()
-    return redirect('/weather')
+    connect.close()
+    return redirect('/tasklist')
 
 @app.route('/add')
 def add():
     return render_template('add.html')
+
+@app.route('/tasklist')
+def tasklist():
+    connect = sqlite3.connect('flasktest.db')
+    cursor = connect.cursor()
+    cursor.execute("SELECT id, task FROM task")
+    task = cursor.fetchall()
+    connect.close()
+    # taskのままだとリスト型の中にたぷるなので使いづらい
+    # そのためリスト型の中に辞書型を持つtask_listを作る！
+    task_list = []
+    for row in task:
+        task_list.append({"id":row[0], "task":row[1]})
+    print(task)
+    print(task_list)
+    return render_template('tasklist.html', html_task = task_list)  
+
+@app.route('/edit/<int:id>')
+def edit(id):
+    connect = sqlite3.connect('flasktest.db')
+    cursor = connect.cursor()
+    cursor.execute("SELECT task FROM task WHERE id = ?",
+    (id,))
+    task = cursor.fetchone()[0]
+    connect.close()
+    item = {"id":id, "task":task}
+    return render_template('edit.html', html_task = item)
+
+@app.route('/edit', methods=["POST"])
+def edt_post():
+    id = request.form.get('id')
+    task = request.form.get('task')
+    connect = sqlite3.connect('flasktest.db')
+    cursor = connect.cursor()
+    cursor.execute("UPDATE task SET task = ? WHERE id = ? ",
+    (task, id))
+    connect.commit()
+    connect.close()
+    return redirect('/tasklist')
 
 if __name__  == "__main__":
     app.run(debug=True)
